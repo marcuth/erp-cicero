05055c9adcb97772c970450ec178a8cceda8443e
Marcuth <thiago.playgames15@gmail.com>
2025-08-05 02:46:58 -0300

    Add window.py

---
+++ src/utils/window.py
@@ -0,0 +1,17 @@
+from tkinter import Tk
+
+def center_window(root: Tk) -> None:
+    root.update_idletasks()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    window_width = root.winfo_width()
+    window_height = root.winfo_height()
+
+    x = (screen_width - window_width) // 2
+    y = (screen_height - window_height) // 2
+
+    root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+    
+def clear_window(window: Tk):
+    for widget in window.winfo_children():
+        widget.destroy()
