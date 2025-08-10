253e2a52088c5c74f8f20a3a173b46ef8168f4dd
Marcuth <thiago.playgames15@gmail.com>
2025-08-10 17:54:23 -0300

    Add bar_code.py

---
+++ src/models/bar_code.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from .base import Base
+
+class ProductBarCode(Base):
+    __tablename__ = "product_barcodes"
+    id = Column(Integer, primary_key=True)
+    code = Column(String(50), nullable=False)
+    product_id = Column(Integer, ForeignKey("products.id"))
+
+    product = relationship("Product", back_populates="bar_codes")
