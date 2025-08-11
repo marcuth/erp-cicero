d6623d0c9b97feaefc72cf75ffacba1893cda938
Marcuth <thiago.playgames15@gmail.com>
2025-08-11 00:26:50 -0300

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+
+from models import Base, BarCode, Customer, Order, OrderItem, User
 from config import DB_URL
 
 engine = create_engine(DB_URL, echo=False, future=True)
@@ -7,5 +9,4 @@ SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
 Base = declarative_base()
 
 def init_db():
-    from models import user, client, product, order, installment
     Base.metadata.create_all(bind=engine)
