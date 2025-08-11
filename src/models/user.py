8d88b036f133522bf95cee7b61029ac90cf1ae8b
Marcuth <thiago.playgames15@gmail.com>
2025-08-11 00:26:51 -0300

    Update user.py

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,7 @@
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Enum
+
+from enums.user_role import UserRole
 from .base import Base
 
 class User(Base):
@@ -8,5 +10,5 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String(150), unique=True, nullable=False)
     name = Column(String(250))
-    role = Column(String(20))
+    role = Column(Enum(UserRole), nullable=False)
     password = Column(String(256))
