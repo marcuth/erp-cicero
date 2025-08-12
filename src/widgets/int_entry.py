764373579ca5d7650898205cbceebcac9b2dc626
Marcuth <thiago.playgames15@gmail.com>
2025-08-11 23:38:21 -0300

    Add int_entry.py

---
+++ src/widgets/int_entry.py
@@ -0,0 +1,15 @@
+from ttkbootstrap.constants import *
+import ttkbootstrap as ttk
+
+class IntEntry(ttk.Entry):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        
+        vcmd = self.register(self._validate_input)
+        self.config(
+            validate="key",
+            validatecommand=(vcmd, '%S')
+        )
+
+    def _validate_input(self, text: str):
+        return text.isdigit() or text == ""
