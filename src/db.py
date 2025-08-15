da90dad23db2adff7df5b894d641fcff75339a03
Marcuth <thiago.playgames15@gmail.com>
2025-08-14 23:16:47 -0300

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
 
 from config import DB_URL
 from models import Base
@@ -10,3 +10,4 @@ Base = declarative_base()
 
 def init_db():
     Base.metadata.create_all(bind=engine)
+    
